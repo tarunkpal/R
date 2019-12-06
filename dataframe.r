5c71b396adfe789d8bc985be785b2a8c5802f7d0
tarunkpal <44465953+tarunkpal@users.noreply.github.com>
2019-12-06 20:29:28 +0530

    Update dataframe.r

--- dataframe.r
+++ dataframe.r
@@ -1,2 +1,20 @@
-data=read.csv("F:\Tarun\data.csv")
+data=read.csv("F:\Tarun\data.csv") /reading csv from local machine
 df=data.frame(data)
+print(df[1:5,])     /printing first 5 entries
+
+/creating new dataframe
+
+df2 <- data.frame(
+  std_id=c(1:5)
+  name=c("Ram","Mohan","Rahul","Modi","Amit")
+  age=c(14,15,14,14,16)
+  class=c(7,7,7,8,8))
+
+
+/adding new column
+
+df2$height <- c(147,150,150,153,145) /height in cm
+write.csv(df2,"F:\Tarun")             /writing dataframe in csv
+
+ondat <- read.csv(url("http://samplecsvs.s3.amazonaws.com/SalesJan2009.csv"))    /reading csv from online source
+print(ondat[1:4,])                                                               /printing frist 4 entries
